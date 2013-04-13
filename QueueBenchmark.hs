1637f7471356e544310dc4a70f00f5a3f2593364
Niklas Hambüchen <mail@nh2.me>
2013-04-13 10:59:25 +0800

    Cleaning up

--- QueueBenchmark.hs
+++ QueueBenchmark.hs
@@ -42,64 +42,72 @@ main = do
                    , bench "create + toList" $ nf (toListPS . makePSQ) randomNumbers
                    , bench "toList" $ nf toListPS preparedPSQ
                    , bench "keys" $ nf PSQ.keys preparedPSQ
-                   , bench "findMin" $ nf (unMaybeBindingPS . PSQ.findMin) preparedPSQ
+                   , bench "findMin" $ nf (fmap toTupPS . PSQ.findMin) preparedPSQ
                    , bench "size" $ nf PSQ.size preparedPSQ
+                   , bench "top 2" $ nf top2PSQ preparedPSQ
                    ]
-     , bgroup "FT" [ bench "create" $ whnf makeFT randomNumbers
+    , bgroup "FT"  [ bench "create" $ whnf makeFT randomNumbers
                    , bench "create + toList" $ nf (toListFT . makeFT) randomNumbers
                    , bench "toList" $ nf toListFT preparedFT
                    , bench "keys" $ nf FT.keys preparedFT
-                   , bench "findMin" $ nf (unMaybeBindingFT . FT.findMin) preparedFT
+                   , bench "findMin" $ nf (fmap toTupFT . FT.findMin) preparedFT
                    , bench "size" $ nf FT.size preparedFT
+                   , bench "top 2" $ nf top2FT preparedFT
                    ]
-     , bgroup "QL" [ bench "create" $ whnf makeQL randomNumbers
+    , bgroup "QL"  [ bench "create" $ whnf makeQL randomNumbers
                    , bench "create + toList" $ nf (toListQL . makeQL) randomNumbers
                    , bench "toList" $ nf toListQL preparedQL
                    -- keys does not exist here
-                   , bench "findMin" $ nf (unMaybeBindingQL . QL.top) preparedQL
+                   , bench "findMin" $ nf (fmap toTupQL . QL.top) preparedQL
                    , bench "size" $ nf QL.size preparedQL
+                   , bench "top 2" $ nf top2QL preparedQL
                    ]
-     , bgroup "GHC" [ bench "create" $ whnf (makeGHC ghcUniques) randomNumbers
-                    , bench "create + toList" $ nf (toListGHC . makeGHC ghcUniques) randomNumbers
-                    , bench "toList" $ nf toListGHC preparedGHC
-                    -- keys does not exist here
-                    , bench "findMin" $ nf (unMaybeElemGHC . GHC.findMin) preparedGHC
-                    , bench "size" $ nf GHC.size preparedGHC
-                    ]
-     ]
+    , bgroup "GHC" [ bench "create" $ whnf (makeGHC ghcUniques) randomNumbers
+                   , bench "create + toList" $ nf (toListGHC . makeGHC ghcUniques) randomNumbers
+                   , bench "toList" $ nf toListGHC preparedGHC
+                   -- keys does not exist here
+                   , bench "findMin" $ nf (fmap toTupGHC . GHC.findMin) preparedGHC
+                   , bench "size" $ nf GHC.size preparedGHC
+                   , bench "top 2" $ nf top2GHC preparedGHC
+                   ]
+    ]
   where
     -- PSQueue
     makePSQ :: [Int] -> PSQ.PSQ Int Double
     makePSQ randomNumbers = PSQ.fromList [ r PSQ.:-> fromIntegral r*2 | r <- randomNumbers ]
 
-    toListPS psq = [ (r, p) | r PSQ.:-> p <- PSQ.toList psq ]
+    toTupPS (k PSQ.:-> p) = (k, p)
 
-    unMaybeBindingPS (Just (k PSQ.:-> p)) = Just (k, p)
-    unMaybeBindingPS Nothing              = Nothing
+    toListPS = map toTupPS . PSQ.toList
+
+    top2PSQ q = (\(m, rest) -> (toTupPS m, toTupPS <$> PSQ.findMin rest)) <$> PSQ.minView q
 
     -- FingerTree.PSQueue
     makeFT :: [Int] -> FT.PSQ Int Double
     makeFT randomNumbers = FT.fromList [ r FT.:-> (fromIntegral r*2) | r <- randomNumbers ]
 
-    toListFT ft = [ (r, p) | r FT.:-> p <- FT.toList ft ]
+    toTupFT (k FT.:-> p) = (k, p)
 
-    unMaybeBindingFT (Just (k FT.:-> p)) = Just (k, p)
-    unMaybeBindingFT Nothing             = Nothing
+    toListFT = map toTupFT . FT.toList
+
+    top2FT q = (\(m, rest) -> (toTupFT m, toTupFT <$> FT.findMin rest)) <$> FT.minView q
 
     -- queuelike PQueue
     makeQL :: [Int] -> QL.PQueue (Int :-> Double)
     makeQL randomNumbers = QL.fromList [ r QL.:-> (fromIntegral r*2) | r <- randomNumbers ]
 
-    toListQL ql = [ (r, p) | r QL.:-> p <- QL.toList ql ]
+    toTupQL (k QL.:-> p) = (k, p)
 
-    unMaybeBindingQL (Just (k QL.:-> p)) = Just (k, p)
-    unMaybeBindingQL Nothing             = Nothing
+    toListQL = map toTupQL . QL.toList
+
+    top2QL q = (\(m, rest) -> (toTupQL m, toTupQL <$> QL.top rest)) <$> QL.extract q
 
     -- GHC.Event.PSQ
     makeGHC :: [Unique] -> [Int] -> GHC.PSQ Int
     makeGHC uniques randomNumbers = GHC.fromList [ GHC.E u (fromIntegral r * 2) r | (u, r) <- zip uniques randomNumbers ]
 
-    toListGHC ghc = [ (r, p) | GHC.E { GHC.prio = p , value = r } <- GHC.toList ghc ]
+    toTupGHC GHC.E { GHC.prio = p, value = r } = (r, p)
 
-    unMaybeElemGHC (Just (GHC.E { GHC.prio = p , value = r })) = Just (r, p)
-    unMaybeElemGHC Nothing                                     = Nothing
+    toListGHC = map toTupGHC . GHC.toList
+
+    top2GHC q = (\(m, rest) -> (toTupGHC m, toTupGHC <$> GHC.findMin rest)) <$> GHC.minView q

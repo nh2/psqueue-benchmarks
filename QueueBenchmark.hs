d5cf891eb55a41bfd9fbcb8676bb4b8d4a43b8b0
Niklas Hambüchen <mail@nh2.me>
2013-04-13 11:00:17 +0800

    Add crash checks

--- QueueBenchmark.hs
+++ QueueBenchmark.hs
@@ -36,6 +36,16 @@ main = do
       preparedQL  = makeQL randomNumbers  :: QL.PQueue (Int :-> Double)
       preparedGHC = makeGHC ghcUniques randomNumbers :: GHC.PSQ Int
 
+
+  putStrLn "Crash checks:"
+  putStrLn $ "PSQ: " ++ findminPSQsequential 1000000 -- works with 5000000
+  putStrLn $ "FT: " ++ findminFTsequential 100000 -- CRASHES with 1000000
+  putStrLn $ "QL: " ++ findminQLsequential 1000000 -- works with 5000000
+  let _GHC_CRASH_N = 100000 -- CRASHES with 1000000
+  crashCheckUniques <- replicateM _GHC_CRASH_N (GHC.newUnique ghcUniqueSource)
+  putStrLn $ "GHC: " ++ findminGHCsequential crashCheckUniques _GHC_CRASH_N
+
+
   defaultMain
     [ bgroup "PSQ" [ -- PSQueue stack overflows for _N = 100000
                      bench "create" $ whnf makePSQ randomNumbers
@@ -45,6 +55,7 @@ main = do
                    , bench "findMin" $ nf (fmap toTupPS . PSQ.findMin) preparedPSQ
                    , bench "size" $ nf PSQ.size preparedPSQ
                    , bench "top 2" $ nf top2PSQ preparedPSQ
+                   , bench "findmin sequential" $ nf findminPSQsequential 10000
                    ]
     , bgroup "FT"  [ bench "create" $ whnf makeFT randomNumbers
                    , bench "create + toList" $ nf (toListFT . makeFT) randomNumbers
@@ -53,6 +64,7 @@ main = do
                    , bench "findMin" $ nf (fmap toTupFT . FT.findMin) preparedFT
                    , bench "size" $ nf FT.size preparedFT
                    , bench "top 2" $ nf top2FT preparedFT
+                   , bench "findmin sequential" $ nf findminFTsequential 10000
                    ]
     , bgroup "QL"  [ bench "create" $ whnf makeQL randomNumbers
                    , bench "create + toList" $ nf (toListQL . makeQL) randomNumbers
@@ -61,6 +73,7 @@ main = do
                    , bench "findMin" $ nf (fmap toTupQL . QL.top) preparedQL
                    , bench "size" $ nf QL.size preparedQL
                    , bench "top 2" $ nf top2QL preparedQL
+                   , bench "findmin sequential" $ nf findminQLsequential 10000
                    ]
     , bgroup "GHC" [ bench "create" $ whnf (makeGHC ghcUniques) randomNumbers
                    , bench "create + toList" $ nf (toListGHC . makeGHC ghcUniques) randomNumbers
@@ -69,12 +82,18 @@ main = do
                    , bench "findMin" $ nf (fmap toTupGHC . GHC.findMin) preparedGHC
                    , bench "size" $ nf GHC.size preparedGHC
                    , bench "top 2" $ nf top2GHC preparedGHC
+                   , bench "findmin sequential" $ nf (findminGHCsequential crashCheckUniques) 10000
                    ]
     ]
   where
     -- PSQueue
     makePSQ :: [Int] -> PSQ.PSQ Int Double
     makePSQ randomNumbers = PSQ.fromList [ r PSQ.:-> fromIntegral r*2 | r <- randomNumbers ]
+
+    findminPSQsequential :: Int -> String
+    findminPSQsequential n = case PSQ.findMin (PSQ.fromList [ show p PSQ.:-> p | p <- [1..n] ]) of
+      Just (k PSQ.:-> p) | p > 0 -> k
+      _                          -> error "findminPSQsequential: should not happen"
 
     toTupPS (k PSQ.:-> p) = (k, p)
 
@@ -86,6 +105,11 @@ main = do
     makeFT :: [Int] -> FT.PSQ Int Double
     makeFT randomNumbers = FT.fromList [ r FT.:-> (fromIntegral r*2) | r <- randomNumbers ]
 
+    findminFTsequential :: Int -> String
+    findminFTsequential n = case FT.findMin (FT.fromList [ show p FT.:-> p | p <- [1..n] ]) of
+      Just (k FT.:-> p) | p > 0 -> k
+      _                         -> error "findminFTsequential: should not happen"
+
     toTupFT (k FT.:-> p) = (k, p)
 
     toListFT = map toTupFT . FT.toList
@@ -96,6 +120,11 @@ main = do
     makeQL :: [Int] -> QL.PQueue (Int :-> Double)
     makeQL randomNumbers = QL.fromList [ r QL.:-> (fromIntegral r*2) | r <- randomNumbers ]
 
+    findminQLsequential :: Int -> String
+    findminQLsequential n = case QL.top (QL.fromList [ show p QL.:-> p | p <- [1..n] ] :: QL.PQueue (String :-> Int)) of
+      Just (k QL.:-> p) | p > 0 -> k
+      _                         -> error "findminQLsequential: should not happen"
+
     toTupQL (k QL.:-> p) = (k, p)
 
     toListQL = map toTupQL . QL.toList
@@ -105,6 +134,11 @@ main = do
     -- GHC.Event.PSQ
     makeGHC :: [Unique] -> [Int] -> GHC.PSQ Int
     makeGHC uniques randomNumbers = GHC.fromList [ GHC.E u (fromIntegral r * 2) r | (u, r) <- zip uniques randomNumbers ]
+
+    findminGHCsequential :: [Unique] -> Int -> String
+    findminGHCsequential uniques n = case GHC.findMin (GHC.fromList [ GHC.E u (fromIntegral p) (show p) | (u, p) <- zip uniques [1..n] ]) of
+      Just (GHC.E _ p v) | p > 0 -> v
+      _                          -> error "findminGHCsequential: should not happen"
 
     toTupGHC GHC.E { GHC.prio = p, value = r } = (r, p)
 
